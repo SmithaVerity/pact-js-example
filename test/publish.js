c36b5a5046896a231187bccb2a2ef1c0f6eb61d5
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-03-13 12:23:35 +0530

    Update publish.js

--- test/publish.js
+++ test/publish.js
@@ -1,17 +1,33 @@
-const pact = require('@pact-foundation/pact-node')
-const path = require('path')
+const pact = require('@pact-foundation/pact-node');
+
+if (!process.env.CI && !process.env.PUBLISH_PACT) {
+    console.log("skipping Pact publish...");
+    process.exit(0)
+}
+
 const pactBrokerUrl = process.env.PACT_BROKER_BASE_URL || 'https://verity.pactflow.io';
 const pactBrokerToken = process.env.PACT_BROKER_TOKEN || 'v_ieq2UfPtJpt1UEt9IsYQ';
 
-pact.publishPacts(opts)
-  .then(() => {
-    console.log('Pact contract publishing complete!')
-    console.log('')
-    console.log('Head over to https://test.pactflow.io/ and login with')
-    console.log('=> Username: dXfltyFMgNOFZAxr8io9wJ37iUpY42M')
-    console.log('=> Password: O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1')
-    console.log('to see your published contracts.')
-  })
-  .catch(e => {
-    console.log('Pact contract publishing failed: ', e)
-  })
+const gitHash = require('child_process')
+    .execSync('git rev-parse --short HEAD')
+    .toString().trim();
+
+const opts = {
+    pactFilesOrDirs: ['./pacts/'],
+    pactBroker: pactBrokerUrl,
+    pactBrokerToken: pactBrokerToken,
+    tags: ['prod', 'test'],
+    consumerVersion: gitHash
+};
+
+pact
+    .publishPacts(opts)
+    .then(() => {
+        console.log('Pact contract publishing complete!');
+        console.log('');
+        console.log(`Head over to ${pactBrokerUrl}`);
+        console.log('to see your published contracts.')
+    })
+    .catch(e => {
+        console.log('Pact contract publishing failed: ', e)
+    });
